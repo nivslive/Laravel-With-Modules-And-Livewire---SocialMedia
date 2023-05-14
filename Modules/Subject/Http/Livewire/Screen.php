22ace5a80a18f5deb66c31176db0a595d9b73742
Nivs <nivsoficial@gmail.com>
2023-05-14 15:01:52 -0300

    last atts about button that send texts (WIP)

--- Modules/Subject/Http/Livewire/Screen.php
+++ Modules/Subject/Http/Livewire/Screen.php
@@ -6,8 +6,19 @@ use Livewire\Component;
 
 class Screen extends Component
 {
+
+    public array $data = [
+        'testando',
+    ];
+    public string $text = '';
     public function render()
     {
-        return view('subject::livewire.screen');
+        return view('subject::livewire.screen', ['data' => $this->data]);
+    }
+
+
+    public function iterateTextInData() {
+        $this->data[] = $this->text;
+        $this->text = '';
     }
 }
--- Modules/Subject/Resources/views/livewire/screen.blade.php
+++ Modules/Subject/Resources/views/livewire/screen.blade.php
@@ -1,4 +1,11 @@
 <div>
     <h3>The <code>Screen</code> livewire component is loaded from the <code>Subject</code> module.</h3>
+    
+    @foreach($data as $d)
+    {{ $d }}
+    @endforeach
 
-    oi</div>
+
+    <input type="text" wire:model="text">
+    <button wire:click="iterateTextInData"> test </button>
+    </div>
